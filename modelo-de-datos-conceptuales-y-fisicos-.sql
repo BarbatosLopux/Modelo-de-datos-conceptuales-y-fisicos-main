1805d9417721a97592551af17010c4c9d61bf52e
BarbatosLopux <sneideresteban@gmail.com>
2023-10-21 13:47:57 -0500

    feat: :tada: Creacion del repositorio

---
+++ modelo-de-datos-conceptuales-y-fisicos-.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `Reservas` DROP FOREIGN KEY `Reservas_fk0`;
+
+ALTER TABLE `Reservas` DROP FOREIGN KEY `Reservas_fk1`;
+
+ALTER TABLE `Propietaros_vehiculos` DROP FOREIGN KEY `Propietaros_vehiculos_fk0`;
+
+ALTER TABLE `Transacciones_de_pago` DROP FOREIGN KEY `Transacciones_de_pago_fk0`;
+
+ALTER TABLE `Transacciones_de_pago` DROP FOREIGN KEY `Transacciones_de_pago_fk1`;
+
